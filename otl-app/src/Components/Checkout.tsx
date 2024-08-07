036a302936c9422ce40a0ee87a97c2fe1a51f829
KennyyyN <96741268+KennyyyN@users.noreply.github.com>
2024-08-07 14:23:58 -0500

    Checkout commit code "dummy"

--- otl-app/src/Components/Checkout.tsx
+++ otl-app/src/Components/Checkout.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+//import React from 'react';
+//import amsServer from '../common/ams-server';
+//import { Form, Button } from 'react-bootstrap';
 
-const Checkout: React.FC = () => {
-    return (
-        <div>
-            <h1>Checkout</h1>
-            {/* Your checkout content goes here */}
-        </div>
-    );
-};
+/*
+(() => {
+    'use strict';
+ 
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms: NodeListOf<HTMLFormElement> = document.querySelectorAll('.needs-validation');
+ 
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form: HTMLFormElement) => {
+      form.addEventListener('submit', (event: Event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+ 
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
+
 
 export default Checkout;
+*/
